34c781cdbf9826f447b75090a9ed1135e1a3e52d
Adnan Hemani <adnan.h@berkeley.edu>
2018-11-19 07:20:22 -0800

    WIP

--- installer.rb
+++ installer.rb
@@ -10,8 +10,10 @@ end
 
 # Set right remote
 git_repo_name = /([^\/]+).git$/.match(`git config --local remote.origin.url`)[1]
+current_git_branch = `git rev-parse --abbrev-ref HEAD`.strip
 current_git_repo_url = `git --git-dir .git_cs169 remote get-url origin`
 if git_repo_name != "git@github.com:saasbook/#{git_repo_name}.git"
     `git --git-dir .git_cs169 remote set-url origin git@github.com:saasbook/#{git_repo_name}.git`
-    `git --git-dir .git_cs169 pull`
+    `git --git-dir .git_cs169 fetch --all`
+    `git --git-dir .git_cs169 reset --hard origin/#{current_git_branch}`
 end
