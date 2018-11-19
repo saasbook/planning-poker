4df9120f047d9fc8f8b1227d3961c00df7ccdf4a
Adnan Hemani <adnan.h@berkeley.edu>
2018-11-19 07:10:19 -0800

    WIP

---
+++ installer.rb
@@ -0,0 +1,17 @@
+require 'fileutils'
+# Check that second git directory is set up correctly
+if not Dir.exists?('.git_cs169')
+    puts "got here"
+    FileUtils.cp_r '.git', '.git_cs169'
+end
+
+# If no related file in config/initializers, then create that file
+
+
+# Set right remote
+git_repo_name = /([^\/]+).git$/.match(`git config --local remote.origin.url`)[1]
+current_git_repo_url = `git --git-dir .git_cs169 remote get-url origin`
+if git_repo_name != "git@github.com:saasbook/#{git_repo_name}.git"
+    `git --git-dir .git_cs169 remote set-url origin git@github.com:saasbook/#{git_repo_name}.git`
+    `git --git-dir .git_cs169 pull`
+end
